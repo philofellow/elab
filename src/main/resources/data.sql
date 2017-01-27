d2b7a6d0f60d0d93c01214098def22b50a8e3ca8
Yanyan Ni <nyy7@Yanyans-Device.local>
2017-01-26 23:27:00 -0500

    correct login.html

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,7 @@ grant create on schema public to postgres;
 alter role postgres set search_path to public;
 set search_path to public;
 
+
 INSERT INTO lab_user(name, email, password, organization, role)
 VALUES ('zhao', 'zhao@zyans.com', 'zhao1234', 'virginia tech', 'USER');
 
